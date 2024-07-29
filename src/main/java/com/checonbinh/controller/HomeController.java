8d3f9354b9b2ec79176d3875fc311b25d6ea28da
samro123 <thsam.20it5@vku.udn.vn>
2024-07-29 14:12:49 +0700

    Tra du lieu len view Home

--- src/main/java/com/checonbinh/controller/HomeController.java
+++ src/main/java/com/checonbinh/controller/HomeController.java
@@ -11,48 +11,36 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.checonbinh.entity.Manager;
 import com.checonbinh.entity.NhanVien;
 
 @Controller
 public class HomeController {
+//	@RequestMapping("/home")
+//	public ModelAndView Home() {
+//		ModelAndView viewHome = new ModelAndView();
+//		viewHome.setViewName("home");
+//		
+//	String username = "Sam";
+//	String password = "123456789";
+//	
+//	viewHome.addObject("username", username);
+//	viewHome.addObject("password", "password");
+//		return viewHome;
+//	}
 	@RequestMapping("/home")
-	@ResponseBody
-	public String Home() {
-		ApplicationContext context = new ClassPathXmlApplicationContext("IoC.xml");
-		NhanVien nhanVien = (NhanVien) context.getBean("nhanvien");
-		System.out.println(nhanVien.getManager().getMoneny() + " " + nhanVien.getManager().getName());
+	public String Home(ModelMap modelMap) {
+		String username = "Sam";
+		String password = "123456789";
 		
-		//Resource resource = context.getResource("https://samapi123.wiremockapi.cloud/storeDetails/1");
-//		CustomLoader customLoader = new CustomLoader();
-//		customLoader.setResourceLoader(context);
-		CustomLoader customLoader = (CustomLoader) context.getBean("customLoader");
-		Resource resource = customLoader.getResource("https://samapi123.wiremockapi.cloud/storeDetails/1");
-		try {
-			InputStream inputStream = resource.getInputStream();
-			InputStreamReader reader = new InputStreamReader(inputStream);
-			BufferedReader bufferedReader = new BufferedReader(reader);
-			String line = "";
-			StringBuilder datas = new StringBuilder();
-			while((line = bufferedReader.readLine()) != null) {
-				datas.append(line + "\n");
-			}
-			
-			bufferedReader.close();
-			reader.close();
-			inputStream.close();
-			
-			System.out.println("Giá trị : " + datas.toString());
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return "abc";
+		modelMap.addAttribute("username", username);
+		modelMap.addAttribute("password", "password");
+		return "home";
 	}
 
 }
